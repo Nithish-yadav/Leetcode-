76b89c29d2b70a18895a3411d047066289844151
Nithish-yadav <mailme9380@gmail.com>
2025-05-25 22:52:05 +0530

    Updated

---
+++ remove_dup.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {1, 1, 2, 3};
+    int i = 0;
+
+    for (int j = 1; j < nums.size(); j++)
+    {
+        if (nums[i] != nums[j])
+        {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    int l = i + 1;
+
+    cout << "Unique elements: ";
+    for (int k = 0; k < l; k++)
+    {
+        cout << nums[k] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
