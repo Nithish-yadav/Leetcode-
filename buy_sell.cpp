5ba3dcd05bd842747aa8264bc0c3c79aeb7f598a
Nithish-yadav <mailme9380@gmail.com>
2025-05-27 23:38:40 +0530

    Buy Sell Optimal Solution

--- buy_sell.cpp
+++ buy_sell.cpp
@@ -3,34 +3,50 @@ using namespace std;
 
 int main()
 {
-    int arr[5] = {2, 4, 1};
+    int arr[6] = {7, 1, 5, 3, 6, 4};
     int size = sizeof(arr) / sizeof(int);
+    int maxProfit = 0, bestBuy = arr[0];
 
-    int min = INT_MAX;
-    int max = INT_MIN;
-    int buyprice;
-    int sellprice;
-    for (int i = 0; i < size; i++)
+    for (int i = 1; i < size; i++)
     {
-
-        if (arr[i] < min)
+        if (arr[i] > bestBuy)
         {
-            min = arr[i];
-            buyprice = i;
+            maxProfit = max(maxProfit, arr[i] - bestBuy);
         }
-    }
-    for (int j = buyprice; j < size; j++)
-    {
-        if (arr[j] > max)
-        {
-            max = arr[j];
-            sellprice = j;
-        }
+        bestBuy = min(bestBuy, arr[i]);
     }
 
-    cout << arr[buyprice] << " " << arr[sellprice] << endl;
-    int profit = 0;
-    profit = arr[sellprice] - arr[buyprice];
-    cout << profit;
+    cout << maxProfit;
     return 0;
 }
+
+// Brute Force
+//  int arr[5] = {2, 4, 1};
+//  int size = sizeof(arr) / sizeof(int);
+
+// int min = INT_MAX;
+// int max = INT_MIN;
+// int buyprice;
+// int sellprice;
+// for (int i = 0; i < size; i++)
+// {
+
+//     if (arr[i] < min)
+//     {
+//         min = arr[i];
+//         buyprice = i;
+//     }
+// }
+// for (int j = buyprice; j < size; j++)
+// {
+//     if (arr[j] > max)
+//     {
+//         max = arr[j];
+//         sellprice = j;
+//     }
+// }
+
+// cout << arr[buyprice] << " " << arr[sellprice] << endl;
+// int profit = 0;
+// profit = arr[sellprice] - arr[buyprice];
+// cout << profit;
