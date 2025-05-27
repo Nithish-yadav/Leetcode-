4a663ecf4b3531036d50421be159ec0f4ef27cb0
Nithish-yadav <mailme9380@gmail.com>
2025-05-28 00:02:30 +0530

    Buy Sell Optimal Solution

--- buy_sell.cpp
+++ buy_sell.cpp
@@ -5,10 +5,11 @@ int main()
 {
     int arr[6] = {7, 1, 5, 3, 6, 4};
     int size = sizeof(arr) / sizeof(int);
-    int maxProfit = 0, bestBuy = arr[0];
+    int maxProfit = 0, bestBuy = arr[0]; // 7
 
     for (int i = 1; i < size; i++)
     {
+
         if (arr[i] > bestBuy)
         {
             maxProfit = max(maxProfit, arr[i] - bestBuy);
@@ -19,6 +20,16 @@ int main()
     cout << maxProfit;
     return 0;
 }
+
+// 1 > 7 F (bestBuy = min(7,1) = 1)
+//  5 > 1 T (maxProfit = max(0, 5-1) = 4) → bestBuy remains 1
+//  3 > 1 T (maxProfit = max(4, 3-1) = 4) → bestBuy remains 1
+// 6 > 1 T (maxProfit = max(4, 6-1) = 5) → bestProfit updated
+// 4 > 1 T (maxProfit = max(5, 4-1) = 5) → no change
+//
+//
+//
+//
 
 // Brute Force
 //  int arr[5] = {2, 4, 1};
