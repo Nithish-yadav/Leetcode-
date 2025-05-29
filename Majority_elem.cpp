ebd54e46606de4128169bea5875b74ba05bcb332
Nithish-yadav <mailme9380@gmail.com>
2025-05-29 18:22:44 +0530

    Move Zeros

---
+++ Majority_elem.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec = {1, 2, 1, 2, 1, 1};
+
+    return 0;
+}
+
+// Brute Force
+//  int n = vec.size();
+//  for (int val : vec)
+//  {
+//      int fre = 0;
+//      for (int el : vec)
+//      {
+//          if (el == val)
+//          {
+//              fre++;
+//          }
+//      }7
+//      if (fre > n / 2)
+//      {
+//          cout << val;
+//      }
+//  }
---
+++ move0.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Two Pointers
+int main()
+{
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    int i = 0;
+    for (int j = 0; j < nums.size(); j++)
+    {
+        if (nums[j] != 0)
+        {
+            swap(nums[i], nums[j]);
+            i++;
+        }
+    }
+
+    for (int val : nums)
+    {
+        cout << val;
+    }
+    return 0;
+}
